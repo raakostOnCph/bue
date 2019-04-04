36ba35865caaedbd4dafa3b35bdc16b811820687
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2019-04-04 11:21:30 +0200

    hej bue

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        udskriv();
     }
+
+    public static void udskriv() {
+        System.out.println("hej bue");
+
+    }
+
 }
